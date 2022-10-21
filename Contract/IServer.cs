69cfcfbe139c9971a45ca3081e8b4f4b3554cc44
Ryan Alba <tgr.superlight@gmail.com>
2022-10-22 02:37:09 +0900

    Initial commit

---
+++ Contract/IServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[ComVisible(true)]
+[Guid(Contract.Constants.ServerInterface)]
+public interface IServer
+{
+    /// <summary>
+    /// Compute the value of the constant Pi.
+    /// </summary>
+    double ComputePi();
+}
